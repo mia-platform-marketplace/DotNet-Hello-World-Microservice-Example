32b2805c681b33c85bd2b194ce4f05be40723435
Giulio Roggero <giulio.roggero@mia-platform.eu>
2020-04-19 11:32:12 +0200

    added credits and swagger doc for Mia-Platform API Portal

--- Dockerfile
+++ Dockerfile
@@ -8,24 +8,20 @@ ENV ASPNETCORE_URLS=http://*:3000
 FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build
 WORKDIR /src
 
-# TODO: interpolate
 COPY HelloWorld.csproj .
-# TODO: interpolate
 RUN dotnet restore HelloWorld.csproj
 COPY . .
 WORKDIR /src/.
-# TODO: interpolate
 RUN dotnet build "HelloWorld.csproj" -c Release -o /app/build
 
 ##########################################################################
 FROM build AS publish
-# TODO: interpolate
 RUN dotnet publish "HelloWorld.csproj" -c Release -o /app/publish
 
 ##########################################################################
 LABEL maintainer="%CUSTOM_PLUGIN_CREATOR_USERNAME%" \
       name="%CUSTOM_PLUGIN_SERVICE_NAME%" \
-      description="%CUSTOM_PLUGIN_SERVICE_DESCRIPTION%" \
+      description="%CUSTOM_PLUGIN_SERVICE_NAME%" \
       eu.mia-platform.url="https://www.mia-platform.eu" \
       eu.mia-platform.version="0.1.0" \
       eu.mia-platform.language="c#" \
@@ -36,7 +32,6 @@ FROM base AS final
 WORKDIR /app
 
 ARG COMMIT_SHA=<not-specified>
-# TODO: interpolate
 RUN echo "%CUSTOM_PLUGIN_SERVICE_NAME%: $COMMIT_SHA" >> ./commit.sha
 
 COPY --from=publish /app/publish .
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +26,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
+// PLEASE NOTE: You don't need to modify this code. Just add a new Controller in Controllers folder
 namespace HelloWorld
 {
     public class Startup
     {
+
+        // The name of entry point form Mia-Platform API Portal
+        private const string DOCUMENTATION_MIA = "documentation";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,26 +44,36 @@ namespace HelloWorld
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Enable swagger
+            services.AddSwaggerGen(c =>
+            {
+                //
+                c.SwaggerDoc(DOCUMENTATION_MIA, new OpenApiInfo { Title = "Hello API", Version = "v1" });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            // Enable debug page
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // Enable OpenAPI Specification for Mia-Platform API Portal
+             app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "/{documentName}/json";
+            });
 
+            // Register routes
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();

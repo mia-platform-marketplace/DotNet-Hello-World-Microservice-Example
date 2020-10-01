6fd8e5fa478253864fe7727189e5d3a6382bf2a1
Grieco <grieco@intre.it>
2020-10-01 18:09:32 +0200

    Setup hello world example

---
+++ DotNetCore_Hello-World-Microservice-Example/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using MiaServiceDotNetLibrary;
+using MiaServiceDotNetLibrary.Decorators;
+using MiaServiceDotNetLibrary.Environment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore_Hello_World_Microservice_Example.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ServiceStatusController
+    {
+        public HelloWorldController(MiaEnvConfiguration miaEnvConfiguration, ServiceClientFactory serviceClientFactory,
+            DecoratorResponseFactory decoratorResponseFactory) : base(miaEnvConfiguration, serviceClientFactory,
+            decoratorResponseFactory)
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var miaHeadersPropagator = (MiaHeadersPropagator) HttpContext.Items["MiaHeadersPropagator"];
+            return $"Hello {miaHeadersPropagator.GetUserId()}";
+        }
+    }
+}
